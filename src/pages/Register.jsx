f974bd30358d4514ad0e46d23456574d1f70c9c2
joyce-esser <joyce-esser@outlook.com>
2023-01-21 19:13:42 +0100

    install git

---
+++ src/pages/Register.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Register = () => {
+    return (
+        <div className="formContainer">
+            <div className="formWrapper">
+                <span className="logo">Talk!</span>
+                <span className ="title">Register</span>
+                <form>
+                    <input type="text" placeholder="display name"/>
+                    <input type="email" placeholder="email"/>
+                    <input type="password" placeholder="password"/>
+                    <input style={{display:"none"}} type="file" id="file" />
+                    <label htmlFor="file" />
+                    asbds
+                    <button>Sign up</button>
+                </form>
+                <p>Do you have already an account? Login</p>
+            </div>
+        </div>
+    )
+}
+
+export default Register
